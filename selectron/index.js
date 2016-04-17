f9c818462e11aa8a534a60848b29f5381563b7e5
BastiTee <basti.tee@gmx.de>
2016-04-17 23:18:19 +0200

    no menu anymore;

--- selectron/index.js
+++ selectron/index.js
@@ -31,7 +31,8 @@ app.on('ready', function() {
         // frame: false // Later to disable any menus
         icon: 'selectron/icon.png'
     });
-
+    mainWindow.setMenu(null);
+    
     // and load the index.html of the app.
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 
